c398b193a0eb5535bcbcbc4f97fa2ffbd6ba27e5
Zezai <2754013417@qq.com>
2023-04-17 19:51:27 +0800

    init

---
+++ src/main/java/com/zezai/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.zezai.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zezai.domain.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
---
+++ src/main/java/com/zezai/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.zezai.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zezai.domain.Employee;
+import com.zezai.mapper.EmployeeMapper;
+import com.zezai.service.EmployeeService;
+
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper,Employee> implements EmployeeService {
+}
